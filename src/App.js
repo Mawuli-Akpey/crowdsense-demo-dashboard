1398d26500744c5e8d7e16ce5ac30c37e5e26af1
Mawuli Akpey <akpeym@gmail.com>
2025-04-26 18:50:34 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CrowdSenseDashboard from './CrowdSenseDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <CrowdSenseDashboard />
+    </div>
+  );
+}
+
+export default App;
